bfb02843473ef01c0e1d339828718c01d7aa4c84
Geeoon Chung <geeoon13@gmail.com>
2021-07-18 14:26:00 -0500

    Added MAX_ITERATIONS constant.

--- JuliaSet/src/main.cpp
+++ JuliaSet/src/main.cpp
@@ -6,6 +6,7 @@ constexpr float GOLDEN_RATIO{ 1.618f };
 constexpr unsigned int XDIM{ 800 };
 constexpr unsigned int YDIM{ 800 };
 constexpr float ESCAPE_RADIUS{ 2.0f };
+constexpr int MAX_ITERATIONS{ 1000 };
 
 int main() {
 	sf::Vector2f C = sf::Vector2f{ 1.0f - GOLDEN_RATIO, 0.0f };
@@ -26,12 +27,12 @@ int main() {
 	UIManager ui{ XDIM, YDIM, mandelbrot_shader };
 
 	mandelbrot_shader->setUniform("viewportSize", sf::Vector2f{ ui.getWindowSize() });
-	mandelbrot_shader->setUniform("maxIteration", 1000);
+	mandelbrot_shader->setUniform("maxIteration", MAX_ITERATIONS);
 
 	julia_shader->setUniform("R", ESCAPE_RADIUS);
 	julia_shader->setUniform("viewportSize", sf::Vector2f{ ui.getWindowSize() });
 	julia_shader->setUniform("c", C);
-	julia_shader->setUniform("maxIteration", 1000);
+	julia_shader->setUniform("maxIteration", MAX_ITERATIONS);
 
 
 	while (ui.isOpen()) {
