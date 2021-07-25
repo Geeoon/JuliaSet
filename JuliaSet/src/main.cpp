ec0176dd0c274056a04ceb05c21734f86960f004
Geeoon Chung <geeoon13@gmail.com>
2021-07-24 21:35:22 -0500

    Renamed shaders.

--- JuliaSet/src/main.cpp
+++ JuliaSet/src/main.cpp
@@ -9,37 +9,38 @@ constexpr float ESCAPE_RADIUS{ 2.0f };
 constexpr int MAX_ITERATIONS{ 1000 };
 
 int main() {
-	sf::Vector2f C = sf::Vector2f{ 1.0f - GOLDEN_RATIO, 0.0f };
+	sf::Vector2f C = sf::Vector2f{ 0.0f, 0.0f };
 	if (!sf::Shader::isAvailable()) {
 		return -1;
 	}
 	
-	sf::Shader* julia_shader{ new sf::Shader };
-	if (!julia_shader->loadFromFile("src/shaders/julia_shader.frag", sf::Shader::Fragment)) {
+	sf::Shader* julia{ new sf::Shader };
+	if (!julia->loadFromFile("src/shaders/julia.frag", sf::Shader::Fragment)) {
 		return -1;
 	}
 
-	sf::Shader* mandelbrot_shader{ new sf::Shader };
-	if (!mandelbrot_shader->loadFromFile("src/shaders/mandelbrot_shader.frag", sf::Shader::Fragment)) {
+	sf::Shader* mandelbrot{ new sf::Shader };
+	if (!mandelbrot->loadFromFile("src/shaders/mandelbrot.frag", sf::Shader::Fragment)) {
 		return -1;
 	}
 
-	UIManager ui{ XDIM, YDIM, mandelbrot_shader };
+	UIManager ui{ XDIM, YDIM, julia};
 
-	mandelbrot_shader->setUniform("viewportSize", sf::Vector2f{ ui.getWindowSize() });
-	mandelbrot_shader->setUniform("maxIteration", MAX_ITERATIONS);
+	mandelbrot->setUniform("viewportSize", sf::Vector2f{ ui.getWindowSize() });
+	mandelbrot->setUniform("maxIteration", MAX_ITERATIONS);
 
-	julia_shader->setUniform("R", ESCAPE_RADIUS);
-	julia_shader->setUniform("viewportSize", sf::Vector2f{ ui.getWindowSize() });
-	julia_shader->setUniform("c", C);
-	julia_shader->setUniform("maxIteration", MAX_ITERATIONS);
+	julia->setUniform("R", ESCAPE_RADIUS);
+	julia->setUniform("viewportSize", sf::Vector2f{ ui.getWindowSize() });
+	julia->setUniform("c", C);
+	julia->setUniform("maxIteration", MAX_ITERATIONS);
 
 
 	while (ui.isOpen()) {
 		ui.update();
 		ui.pollEvents();
-		//C.y += 0.00001f;
-		julia_shader->setUniform("c", C);
+		C.y += 0.0001f;
+		C.x += 0.0001f;
+		julia->setUniform("c", C);
 	}
 	return 0;
 }
